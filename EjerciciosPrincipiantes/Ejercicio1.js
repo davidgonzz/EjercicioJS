52e3f15465e824f9c951dddb6b8881035b3ef854
David Gonzalez de Gracia <davidgonzalezg2004@gmail.com>
2025-03-27 16:50:30 +0100

    Primer Commit

---
+++ EjerciciosPrincipiantes/Ejercicio1.js
@@ -0,0 +1,11 @@
+// Ejercicio: Suma de dos números
+
+// Solicitar al usuario que ingrese dos números
+let numero1 = parseFloat(prompt("Ingresa el primer número:"));
+let numero2 = parseFloat(prompt("Ingresa el segundo número:"));
+
+// Sumar los dos números
+let suma = numero1 + numero2;
+
+// Mostrar el resultado en la consola
+console.log(`La suma de ${numero1} y ${numero2} es: ${suma}`);
