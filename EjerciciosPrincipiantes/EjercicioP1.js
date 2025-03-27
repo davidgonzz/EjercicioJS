360694dab5fa0f1ae693df4891a783c4bec39ce6
David Gonzalez de Gracia <davidgonzalezg2004@gmail.com>
2025-03-27 18:33:57 +0100

    Traspaso Info

--- EjerciciosPrincipiantes/EjercicioP1.js
+++
@@ -1,9 +0,0 @@
-// Ejercicio 1: Suma de dos números
-// Este programa suma dos números y muestra el resultado en la consola.
-
-const numero1 = 5; // Primer número
-const numero2 = 8; // Segundo número
-
-const suma = numero1 + numero2; // Realizamos la suma
-
-console.log("La suma de", numero1, "y", numero2, "es:", suma);
